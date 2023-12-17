6e1e7587de24953469ddd0f0b862ceeff2ebc3d9
michelalmeida <michelalmeida>
2023-12-17 10:24:22 -0300

    update setup.sh

---
+++ config-git.sh
@@ -0,0 +1,11 @@
+echo "Configuring git"
+
+echo "Configuring default name and e-mail"
+git config --global user.email "michelalmeida"
+git config --global user.name "michelalmeida"
+
+echo "Configuring default branch"
+git config --global init.defaultBranch main
+
+
+
